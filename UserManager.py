6f69a4687fe09db87651f2c6d5984ba338329de0
RafaelRSCaldeira <158341781+RafaelRSCaldeira@users.noreply.github.com>
2024-05-15 23:14:45 -0300

    Update UserManager.py

--- UserManager.py
+++ UserManager.py
@@ -3,13 +3,6 @@ from User import User
 from Undo import Undo
 
 class UserManager(UserManagerWithoutUndo):
-    __instance = None
-
-    def __new__(cls, *args, **kwargs):
-        if cls.__instance is None:
-            cls.__instance = object.__new__(cls, *args, **kwargs)
-        return cls.__instance
-
     def __init__(self):
         self.users = dict()
 
